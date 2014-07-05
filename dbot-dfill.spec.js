a28e7b0ff0abc52d47b0b6dac7e27555ed6228a2
neemzy <tom.panier@free.fr>
2014-07-05 13:21:12 +0200

    fixed tests

--- dbot-dfill.spec.js
+++ dbot-dfill.spec.js
@@ -1,15 +1,11 @@
-var AsyncSpec = require('jasmine-async')(jasmine)
-  , dfill = require('./dbot-dfill');
+var dfill = require('./dbot-dfill');
 
 // Mock regex matches
 var nb = 10
   , matches = ['', nb];
 
 describe('dfill', function () {
-  var async = new AsyncSpec(this)
-    , asyncResult;
-
-  async.it('should retrieve some content', function (done) {
+  it('should retrieve some content', function (done) {
     dfill.callback(matches).then(function (result) {
       expect(typeof result).toEqual('string');
       expect(result.length).toBeGreaterThan(nb);
